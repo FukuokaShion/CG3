baa2f038f2b28b8f1ea7ded8f27f85d64b841775
Fukuoka_Shion <k021g1250@g.neec.ac.jp>
2022-11-27 17:44:56 +0900

    テクスチャ付き四角形

--- Resources/shaders/BasicGeometryShader.hlsl
+++ Resources/shaders/BasicGeometryShader.hlsl
@@ -1,35 +1,34 @@
 #include"BasicShaderHeader.hlsli"
 
-[maxvertexcount(3)]
+static const uint vnum = 4;
+
+static const float4 offset_array[vnum] = {
+	float4(-0.5f, -0.5f,0,0),
+	float4(-0.5f, +0.5f, 0, 0),
+	float4(+0.5f, -0.5f, 0, 0),
+	float4(+0.5f, +0.5f, 0, 0)
+};
+
+static const float2 uv_array[vnum] = {
+	float2(0,1),
+	float2(0,0),
+	float2(1,1),
+	float2(1,0)
+};
+
+[maxvertexcount(vnum)]
 void main(
 	point VSOutput input[1] : SV_POSITION, 
 	inout TriangleStream< GSOutput > output
 )
 {
 	GSOutput element;
-
-	/*for (uint i = 0; i < 3; i++)
+	for (uint i = 0; i < vnum; i++)
 	{
-		GSOutput element;
-		element.svpos = input[i].svpos;
-		element.normal = input[i].normal;
-		element.uv = input[i].uv;
+		element.svpos = input[0].pos + offset_array[i];
+
+		element.svpos = mul(mat, element.svpos);
+		element.uv = uv_array[i];
 		output.Append(element);
-	}*/
-
-	//‹¤’Ê
-	element.normal = input[0].normal;
-	element.uv = input[0].uv;
-
-	//1“_–Ú
-	element.svpos = input[0].svpos;
-	output.Append(element);
-
-	//2“_–Ú
-	element.svpos = input[0].svpos + float4(10.0f, 10.0f, 0, 0);
-	output.Append(element);
-
-	//3“_–Ú
-	element.svpos = input[0].svpos + float4(10.0f, 0, 0, 0);
-	output.Append(element);
+	}
 }
